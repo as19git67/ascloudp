97f0a96b70307ea7fde6f43ec1465d6dcf00bec9
Anton Schegg <as5@schegg.de>
2014-07-26 21:06:28 +0200

    user liste: login Provider in eine Zeile

--- routes/usermanagement.js
+++ routes/usermanagement.js
@@ -7,7 +7,7 @@ var UserLogin = model.models.UserLogin;
 var passportStrategies = require('../passportStrategies');
 
 /* GET user list page. */
-router.get('/',passportStrategies.ensureAuthenticated, function (req, res) {
+router.get('/', passportStrategies.ensureAuthenticated, function (req, res) {
         var appName = config.get('appName');
         var title = 'User Management - Benutzer';
 
@@ -15,31 +15,23 @@ router.get('/',passportStrategies.ensureAuthenticated, function (req, res) {
             .then(function (userlist) {
                 var users = [];
                 userlist.each(function (user) {
+                    var userObj = {
+                        User_id: user.get('id'),
+                        Email: user.get('Email'),
+                        UserName: user.get('UserName'),
+                        LoginProvider: []
+                    };
 
                     var userLogins = user.related('UserLogin');
                     if (userLogins.length > 0) {
                         userLogins.each(function (userLogin) {
-                            var userObj = {
-                                User_id: user.get('id'),
-                                Email: user.get('Email'),
-                                UserName: user.get('UserName'),
-                                UserLogin_id: userLogin.get('id'),
-                                LoginProvider: userLogin.get('LoginProvider'),
-                                ProviderKey: userLogin.get('ProviderKey')
-                            };
-                            users.push(userObj);
+                            userObj.LoginProvider.push(userLogin.get('LoginProvider'));
                         });
-                    } else {
-                        var userObj = {
-                            User_id: user.get('id'),
-                            Email: user.get('Email'),
-                            UserName: user.get('UserName')
-                        };
-                        users.push(userObj);
                     }
+                    users.push(userObj);
                 });
                 res.render('usermanagementlist', {
-                  csrfToken: req.csrfToken(),
+                    csrfToken: req.csrfToken(),
                     appName: appName,
                     title: title,
                     user: req.user,
@@ -49,7 +41,7 @@ router.get('/',passportStrategies.ensureAuthenticated, function (req, res) {
             })
             .catch(function (error) {
                 res.render('usermanagementlist', {
-                      csrfToken: req.csrfToken(),
+                        csrfToken: req.csrfToken(),
                         appName: appName,
                         title: title,
                         user: req.user,
