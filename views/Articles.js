3a037e56d41c87dc2e7b9c51a6816d9771804f5f
Anton Schegg <as5@schegg.de>
2014-09-15 07:25:05 +0200

    SQL für Artikel

--- views/Articles.js
+++ views/Articles.js
@@ -4,23 +4,19 @@ var model = require('../model');
 var moment = require('moment');
 var rho = require('rho');
 var Article = model.models.Article;
+var ArticleItem = model.models.ArticleItem;
 
 var appName = config.get('appName');
 
 module.exports.render = function (req, res, next, page, pages, collectionModelClass) {
     var now = new Date();
-    new Article().query(function (qb) {
-        qb.innerJoin('ArticleItems', 'Articles.id', 'ArticleItems.Article_id');
-        qb.leftJoin('ArticleSections', 'Articles.id', 'ArticleSections.Article_id')
-            .innerJoin('ArticleSectionItems', 'ArticleSections.id','ArticleSectionItems.ArticleSection_id');
-        qb.leftJoin('ArticleReferences', 'ArticleSections.id', 'ArticleReferences.ArticleSection_id')
-            .innerJoin('ArticleReferenceItems', 'ArticleReferences.id','ArticleReferenceItems.ArticleReference_id');
-        qb.orderBy('publish_start', 'DESC');
-        qb.orderBy('ArticleItems.Article_id', 'ASC');
-        qb.where({ 'Page_id': page.Name, 'ArticleItems.valid_end': null, 'ArticleSectionItems.valid_end': null, 'ArticleItems.valid_end': null})
+
+    new ArticleItem().query(function (qb) {
+        qb.innerJoin('Articles', 'Articles.id', 'ArticleItems.Article_id');
+        qb.where({ 'Page_id': page.Name, 'valid_end': null})
             .andWhere('publish_start', '<=', now)
             .andWhere('publish_end', '>=', now);
-        qb.select('*');
+        qb.orderBy('publish_start', 'DESC');
     }).fetchAll().then(function (dataCollection) {
         var records = [];
         if (dataCollection && dataCollection.length > 0) {
@@ -74,7 +70,7 @@ module.exports.render = function (req, res, next, page, pages, collectionModelCl
             });
         }
     }).catch(function (error) {
-        console.log("Error while retrieving Articles from the database: " + error);
+        console.log("Error while retrieving ArticleItems from the database: " + error);
         var err = new Error(error);
         err.status = 500;
         next(err);
