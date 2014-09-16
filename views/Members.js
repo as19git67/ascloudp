4be29ee3c3fdaf7079efa99827e77197555b2430
Anton Schegg <as5@schegg.de>
2014-09-16 17:01:21 +0200

    Mitgliederliste auf neue Tabellen angepasst

--- views/Members.js
+++ views/Members.js
@@ -3,20 +3,24 @@ var moment = require('moment');
 var config = require('../config');
 var model = require('../model');
 var Person = model.models.Person;
+var PersonItem = model.models.PersonItem;
 var Membership = model.models.Membership;
+var MembershipItem = model.models.MembershipItem;
 
 var appName = config.get('appName');
 
 module.exports.render = function (req, res, next, page, pages, collectionModelClass) {
 
-    new Person().query(function (qb) {
+    new PersonItem().query(function (qb) {
+        qb.leftJoin('Persons', 'Persons.id', 'PersonItems.Person_id');
+        qb.leftJoin('Memberships', 'Persons.id', 'Memberships.Person_id');
+        qb.leftJoin('MembershipItems', 'Memberships.id', 'MembershipItems.Membership_id');
         qb.orderBy('Lastname', 'ASC');
-        qb.where({ valid_end: null, 'Membership.LeavingDate': null});
-    }).fetchAll({withRelated: 'Membership'}).then(function (dataCollection) {
+        qb.where({ 'PersonItems.valid_end': null, 'MembershipItems.valid_end': null, 'MembershipItems.LeavingDate': null});
+    }).fetchAll().then(function (dataCollection) {
         var records = [];
         if (dataCollection && dataCollection.length > 0) {
             records = dataCollection.map(function (dataModel) {
-                var membership = dataModel.related('Membership');
                 var dataObj = {
                     Firstname: dataModel.get('Firstname'),
                     Lastname: dataModel.get('Lastname'),
