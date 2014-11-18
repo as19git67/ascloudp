b37ef8d0372c5f99847657996007b7e1e36a11c9
anton <anton@schegg.de>
2014-11-18 12:54:52 +0100

    render after fetch

--- public/javascripts/CalendarApp.js
+++ public/javascripts/CalendarApp.js
@@ -1,6 +1,6 @@
 var CalendarItem = Backbone.Model.extend({
-    urlRoot : 'api/v1',
-    url : function() {
+    urlRoot: 'api/v1',
+    url: function () {
         // Important! It's got to know where to send its REST calls.
         // In this case, POST to '/calendarItems' and PUT to '/calendarItems/:id'
         return this.id ? this.urlRoot + '/events/' + this.id : this.urlRoot + '/events';
@@ -8,24 +8,41 @@ var CalendarItem = Backbone.Model.extend({
 });
 
 
-var CalendarItemView = Backbone.Marionette.ItemView.extend({});
-
-var myView = new CalendarItemView({
+var CalendarItemView = Backbone.Marionette.ItemView.extend({
     template: Handlebars.compile($('*[data-template-name="calendarItem"]').html()),
-    el: '#calendarItemView'
+    el: '#calendarItemView',
+    initialize: function () {
+        if (this.model) {
+            console.log("model event handler set");
+            this.model.on("change", this.render);
+        }
+    }
+    /*,
+    modelEvents: {
+        'change': "modelChanged"
+    },
+    modelChanged: function () {
+        console.log(this.model);
+       // this.render();
+    }
+    */
 });
+
+var myView = new CalendarItemView;
 
 
 $(".calendarListItem").click(function () {
     var clickedElement = $(this);
     var id = clickedElement.attr('data-id');
-    var model = new CalendarItem({ id: id});
-    myView.model = model;
-    model.fetch();
-//    myView.render();
+    var model = new CalendarItem({id: id});
+    var myView = new CalendarItemView({model: model});
+    model.fetch().done(function () {
+        console.log("model fetched");
+        //myView.render();
+    });
 
     $('#editCalendarEntry').on('shown.bs.modal', function (e) {
     });
-    $('#editCalendarEntry').modal({ backdrop: true });
+    $('#editCalendarEntry').modal({backdrop: true});
 
 });
