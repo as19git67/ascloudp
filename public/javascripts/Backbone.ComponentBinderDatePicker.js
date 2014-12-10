6dc7def8a88d7f8be581436614914f5353f144bb
anton <anton@schegg.de>
2014-12-10 12:09:36 +0100

    componentBinder

--- public/javascripts/Backbone.ComponentBinderDatePicker.js
+++ public/javascripts/Backbone.ComponentBinderDatePicker.js
@@ -1,123 +1,45 @@
-(function(root, factory) {
-
-    // Set up ComponentBinderDatePicker appropriately for the environment. Start with AMD.
+(function (factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
-            // Export global even in AMD case in case this script is loaded with
-            // others that may still expect a global ComponentBinderDatePicker.
-            root.ComponentBinderDatePicker = factory(root, exports, _, $);
-        });
+        // AMD. Register as an anonymous module.
+        define(['underscore', 'jquery', 'backbone', 'Backbone.ModelBinder', 'Backbone.ComponentBinder'], factory);
+    }
+    else if(typeof module !== 'undefined' && module.exports) {
+        // CommonJS
+        module.exports = factory(
+            require('underscore'),
+            require('jquery'),
+            require('backbone')
+        );
+    }
+    else {
+        // Browser globals
+        factory(_, $, Backbone);
+    }
+}
+(function(_, $, Backbone){
 
-        // Next for Node.js or CommonJS. jQuery may not be needed as a module.
-    } else if (typeof exports !== 'undefined') {
-        var _ = require('underscore');
-        factory(root, exports, _);
-
-        // Finally, as a browser global.
-    } else {
-        root.ComponentBinderDatePicker = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
+    if(!Backbone.ComponentBinder){
+        throw 'Please include Backbone.ComponentBinder.js before Backbone.ComponentBinderDatePicker.js';
     }
 
-}(this, function(root, ModelBinderElements, _, $) {
-
-    // Initial Setup
-    // -------------
-
-    // Save the previous value of the `ComponentBinderDatePicker` variable, so that it can be
-    // restored later on, if `noConflict` is used.
-    var previousModelBinderElements = root.ComponentBinderDatePicker;
-
-
-    // Current version of the library.
-    ModelBinderElements.VERSION = '1.0.0';
-
-    // For ComponentBinderDatePicker purposes, jQuery, Zepto or Ender owns
-    // the `$` variable.
-    ModelBinderElements.$ = $;
-
-    // Runs ComponentBinderDatePicker in *noConflict* mode, returning the `ComponentBinderDatePicker` variable
-    // to its previous owner. Returns a reference to this ComponentBinderDatePicker object.
-    ModelBinderElements.noConflict = function () {
-        root.ComponentBinderDatePicker = previousModelBinderElements;
-        return this;
-    };
-
-    var BindElement = ModelBinderElements.BindElement = function(el, options) {
-        if (el && obj instanceof jQuery) {
-            this.el = el;
-            options || (options = {});
-
-            this.language = options.language ? options.language : navigator ? navigator.language || navigator.userLanguage : undefined;
-            this.initialize.apply(this, arguments);
-        }
-    };
-
-    // Attach all inheritable methods to the BindElement prototype.
-    _.extend(BindElement.prototype, {
-        // Initialize is an empty function by default. Override it with your own
-        // initialization logic.
-        initialize: function(){},
-
-        getValue: function() {
-             return this.el.val();
+    var ComponentBinderDatePicker = Backbone.ComponentBinder.extend({
+        isResponsibleFor: function(el) {
+            if (el instanceof $)
+            {
+                if (el.hasClass('date')) {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            } else {
+                console.log("el is not a jquery element");
+                return false;
+            }
         },
-        setValue: function(value) {
-            this.el.val(value);
-        },
-        enable: function() {
-            this.el.prop('disabled', false);
-        },
-        disable: function() {
-            this.el.prop('disabled', true);
-        }
-    });
-
-
-
-        // Helpers
-    // -------
-
-    // Helper function to correctly set up the prototype chain, for subclasses.
-    // Similar to `goog.inherits`, but uses a hash of prototype properties and
-    // class properties to be extended.
-    var extend = function(protoProps, staticProps) {
-        var parent = this;
-        var child;
-
-        // The constructor function for the new subclass is either defined by you
-        // (the "constructor" property in your `extend` definition), or defaulted
-        // by us to simply call the parent's constructor.
-        if (protoProps && _.has(protoProps, 'constructor')) {
-            child = protoProps.constructor;
-        } else {
-            child = function(){ return parent.apply(this, arguments); };
-        }
-
-        // Add static properties to the constructor function, if supplied.
-        _.extend(child, parent, staticProps);
-
-        // Set the prototype chain to inherit from `parent`, without calling
-        // `parent`'s constructor function.
-        var Surrogate = function(){ this.constructor = child; };
-        Surrogate.prototype = parent.prototype;
-        child.prototype = new Surrogate;
-
-        // Add prototype properties (instance properties) to the subclass,
-        // if supplied.
-        if (protoProps) _.extend(child.prototype, protoProps);
-
-        // Set a convenience property in case the parent's prototype is needed
-        // later.
-        child.__super__ = parent.prototype;
-
-        return child;
-    };
-
-    BindElement.extend = extend;
-
-
-    var DatePickerBindElement = ModelBinderElements.DatePickerBindElement = BindElement.extend({
         initialize: function(){
+            this.language = options.language ? options.language : navigator ? navigator.language || navigator.userLanguage : undefined;
             var self = this;
             this.el.datetimepicker({language: this.language, pickTime: false});
             this.dateValueAsMoment = this.el.data("DateTimePicker").getDate();
@@ -156,7 +78,9 @@
         }
     });
 
+    // add this component binder to the registry
+    Backbone.ComponentBinder.AddComponentBinder(ComponentBinderDatePicker);
 
-    return ModelBinderElements;
+    return ComponentBinderDatePicker;
 
 }));
