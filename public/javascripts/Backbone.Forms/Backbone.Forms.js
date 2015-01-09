ebe152a191de903196f4f80f73687e2c485d044b
anton <anton@schegg.de>
2015-01-09 15:47:40 +0100

    new: backbone based forms library

--- public/javascripts/Backbone.Forms/Backbone.Forms.js
+++ public/javascripts/Backbone.Forms/Backbone.Forms.js
@@ -1,7 +1,7 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['underscore', 'jquery', 'backbone', 'Backbone.ModelBinder'], factory);
+        define(['underscore', 'jquery', 'backbone', 'handlebars'], factory);
     }
     else if (typeof module !== 'undefined' && module.exports) {
         // CommonJS
@@ -16,71 +16,45 @@
         factory(_, $, Backbone);
     }
 }
-(function (_, $, Backbone) {
+(function (_, $, Backbone, Handlebars) {
 
     if (!Backbone) {
-        throw 'Please include Backbone.js before Backbone.ModelBinder.js';
+        throw 'Please include Backbone.js before Backbone.Forms.js';
+    }
+    if (!Handlebars) {
+        throw 'Please include Handlebars.js before Backbone.Forms.js';
     }
 
-    Backbone.ComponentBinder = function (el, options) {
+    Backbone.Forms = function (model, options) {
         _.bindAll.apply(_, [this].concat(_.functions(this)));
-        this._el = el;
+
+        this._model = model;
         this._options = options;
         if (!this._options) {
             this._options = {};
         }
-    };
 
-    var _componentBinders = Backbone.ComponentBinder._componentBinders = [];
-
-    // static function to add component binders to the "registry" of component binders
-    Backbone.ComponentBinder.AddComponentBinder = function (ComponentBinder) {
-        if (ComponentBinder.prototype instanceof Backbone.ComponentBinder) {
-            _componentBinders.push(ComponentBinder);
+        if (this._options.template && this._options.template instanceof Handlebars) {
+            this._templateGenerationNecessary = false;
+        }
+        else {
+            this._templateGenerationNecessary = true;
         }
     };
 
-    // static function to get a component binder instance that is responsible for the given jquery element
-    Backbone.ComponentBinder.GetComponentBinder = function (el, options) {
-        var componentBinderCount;
-        for (componentBinderCount = 0; componentBinderCount < _componentBinders.length; componentBinderCount++) {
-            var componentBinder = new _componentBinders[componentBinderCount](el, options);
-            if (componentBinder.isResponsible()) {
-                return componentBinder;
-            }
-        }
-        return undefined;
-    };
+    Backbone.Forms.VERSION = '1.0.0';
 
-    Backbone.ComponentBinder.VERSION = '1.0.0';
-
-    _.extend(Backbone.ComponentBinder.prototype, Backbone.Events, {
+    _.extend(Backbone.Forms.prototype, Backbone.Events, {
         // Initialize is an empty function by default. Override it with your own
         // initialization logic.
         initialize: function () {
         },
-        isResponsible: function () {
-            return false;
-        },
-        getValue: function () {
-            return this.el.val();
-        },
-        handleModelChanged: function (model, attributeName) {
-        },
-        setValue: function (value) {
-            var oldValue = this._el.val();
-            this._el.val(value);
-            if (oldValue != value) {
-                this.trigger('change', this);
+        show: function() {
+            if (this._templateGenerationNecessary) {
+                generateTemplateFrom
             }
-        },
-        enable: function () {
-            this._el.prop('disabled', false);
-        },
-        disable: function () {
-            this._el.prop('disabled', true);
         }
     });
 
-    Backbone.ComponentBinder.extend = Backbone.View.extend; // "re-use" backbone helper
+    Backbone.Forms.extend = Backbone.View.extend; // "re-use" backbone helper
 }));
---
+++ public/javascripts/nls/root/lang.js
@@ -0,0 +1,16 @@
+define({
+  version: "1.0",
+  locale: "en-US",
+  language: "en",
+  country: "US",
+  title: "English ( United States )",
+  Country: "United States", // obsolete
+
+  // date formats
+  DefaultDateFormat: "",
+
+  noRelatedParents: "No related parent items available.",
+  noRelatedChildren: "No related child items available.",
+  completeReference: "Complete Reference",
+  showMore: "Show More"
+});
