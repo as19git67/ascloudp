8fd262ace74035ac1245259d627cec6e1f4eeae9
Anton Schegg <anton@schegg.de>
2015-01-16 09:17:22 +0100

    Versuch mit Angular.js

--- public/javascripts/ArticlesApp.js
+++ public/javascripts/ArticlesApp.js
@@ -12,268 +12,99 @@ $.extend($.expr[":"], {
         }
 });
 
-// Save a copy of the old Backbone.sync function so it can be called later.
-var oldBackboneSync = Backbone.sync;
+// create the application module
+var articleEditApp = angular.module('articleEditApp', []);
 
-// Globally override Backbone.Sync
-Backbone.sync = function (method, model, options) {
-    var self = this;
+// setup angularjs resource to use the rest api
+/*
+ angular.module('articleEditApp.services').factory('Entry', function ($resource) {
+ return $resource('/api/v1/articles/:id'); // Note the full endpoint address
+ });
+ */
 
-    // call this function and then the one that was specified in the options
-    // This function resets _isDirty and stores the CSRF token from the header in this.csrfToken.
-    var success = options.success;
-    options.success = function (model, resp, jqXHR) {
-        success && success(model, resp, jqXHR);   // call the original success function
-        self._isDirty = false;      // clear isDirty after each successful sync
-        self.csrfToken = jqXHR.getResponseHeader('X-CSRF-Token');
-    };
+angular.module('articleEditApp.models', []).service('ArticleManager',
+    [
+        '$q',
+        '$http',
+        'Article',
+        function ($q, $http, Article) {
+            return {
+                get: function (id) {
+                    var deferred = $q.defer();
 
-    // on every ajax request add the csrf token to the header unless its a fetch
-    options.beforeSend = function (xhr) {
-        if (method != 'fetch') {
-            if (self.csrfToken) {
-                xhr.setRequestHeader("X-CSRF-Token", self.csrfToken);
+                    $http.get('/api/v1/articles/' + id).success(function (data) {
+                        var articles = [];
+                        for (var i = 0; i < data.objects.length; i++) {
+                            articles.push(new Article(data.objects[i]));
+                        }
+                        deferred.resolve(articles);
+                    });
+
+                    return deferred.promise;
+                }
+            };
+        }
+    ]).factory('Article',
+    function () {
+        function Article(data) {
+            for (var attr in data) {
+                if (data.hasOwnProperty(attr)) {
+                    this[attr] = data[attr];
+                }
             }
         }
-    };
-    return oldBackboneSync.apply(this, [method, model, options]);
+
+        return Article;
+    }
+);
+
+articleEditApp.controller('articleEditController', ['$scope', 'ArticleManager', function ($scope, ArticleManager) {
+        $scope.loadArticle = function (id) {
+            ArticleManager.get(id).then(function (articles) {
+                $scope.article = articles[0];
+            });
+        };
+
+        $scope.loadArticle(id);
+    }]
+);
+
+var ui = {
+    editArticleEntry: $("#editArticleEntry"),
+    errorMessage: $("#errorMessage")
 };
 
-
-var ArticleItem = Backbone.Model.extend({
-    urlRoot: 'api/v1/articles',    // note: backbone adds id automatically
-    _isDirty: false,
-    initialize: function () {
-
-        // If you extend this model, make sure to call this initialize method
-        // or add the following line to the extended model as well
-        this.listenTo(this, 'change', this.modelChanged);
-    },
-    modelChanged: function () {
-        console.log("model changed");
-        this._isDirty = true;
-    },
-    isDirty: function () {
-        return this._isDirty;
-    }
-});
-
-var ArticleItemView = Backbone.Marionette.ItemView.extend({
-    template: Handlebars.compile($('*[data-template-name="articleItem"]').html()),
-    el: '#articleItemView',
-    events: {
-        "click #btSave": "saveClicked"
-    },
-    ui: {
-        editArticleEntry: "#editArticleEntry",
-        errorMessage: "#errorMessage"
-    },
-    initialize: function () {
-    },
-    onRender: function () {
-        this.locale = navigator.language || navigator.userLanguage;
-
-        var model = this.model;
-        var locale = this.locale;
-
-        this.ui.editArticleEntry.on('shown.bs.modal', function (e) {
-            moment.locale(locale);
-            var localeData = moment.localeData();
-            var longDateFormat = localeData._longDateFormat.L;
-
-            var schema = {
-                "type": "object",
-                "properties": {}
-            };
-            var options = {
-                "fields": {}
-            };
-            //console.log("MODEL: ", model);
-
-            function setAlpacaField(fieldSchema, propertyObject, option) {
-                switch (fieldSchema.type) {
-                    case "integer":
-                        propertyObject.type = "integer";
-                        break;
-                    case "character varying":
-                        propertyObject.type = "string";
-                        if (option) {
-                            option.size = fieldSchema.maxLength ? fieldSchema.maxLength : 10;
-                            if (option.size > 255) {
-                                option.type = "textarea";
-                                option.cols = 80;
-                                option.rows = Math.round(option.size / option.cols);
-                                if (option.rows > 10) {
-                                    option.rows = 10;
-                                }
-                            }
-                        }
-                        break;
-                    case "timestamp with time zone":
-                        propertyObject.format = "date";
-                        if (option) {
-                            option.type = "date";
-                            option.dateFormat = longDateFormat;
-                            option.size = longDateFormat.length;
-                        }
-                }
-                propertyObject.required = !fieldSchema.nullable;
-                propertyObject.title = fieldSchema.label;
-                if (option) {
-                    option.placeholder = fieldSchema.description;
-                }
-            }
-
-            _.each(model.get('article'), function (attr) {
-                if (attr instanceof Object) {
-                    var value = attr.value;
-                    var fieldSchema = attr.schema;
-                    var prop = {};
-                    schema.properties[fieldSchema.name] = prop;
-                    var option = {};
-                    options.fields[fieldSchema.name] = option;
-                    setAlpacaField(fieldSchema, prop, option);
-                }
-            });
-
-            _.each(model.get('article_sections'), function (sect) {
-                var sectionName = 'section' + sect.section_id;
-
-                schema.properties[sectionName] = {
-                    type: "object",
-                    properties: {}
-                };
-
-                options.fields[sectionName] = {
-                    label: 'Abschnitt ' + sect.section_id,
-                    fields: {}
-                };
-
-                _.each(Object.keys(sect), function (key) {
-                    var attr = sect[key];
-                    if (attr instanceof Object) {
-                        var value = attr.value;
-                        var fieldSchema = attr.schema;
-                        var prop = {};
-                        schema.properties[sectionName].properties[fieldSchema.name] = prop;
-                        var option = {};
-                        options.fields[sectionName].fields[fieldSchema.name] = option;
-                        setAlpacaField(fieldSchema, prop, option);
-                    }
-                });
-            });
-
-            console.log("SCHEMA", schema);
-            console.log("OPTIONS", options);
-
-            Alpaca.setDefaultLocale(locale);
-            $("#form").alpaca({
-                "schema": schema,
-                "options": options,
-                "view": {
-                    "parent": "bootstrap-create-horizontal",
-                    "messages": {
-                        "de": {
-                            required: "Eingabe erforderlich",
-                            invalid: "Eingabe ungültig",
-                            months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
-                            timeUnits: {
-                                SECOND: "Sekunden",
-                                MINUTE: "Minuten",
-                                HOUR: "Stunden",
-                                DAY: "Tage",
-                                MONTH: "Monate",
-                                YEAR: "Jahre"
-                            },
-                            "notOptional": "Dieses Feld ist nicht optional",
-                            "disallowValue": "Diese Werte sind nicht erlaubt: {0}",
-                            "invalidValueOfEnum": "Diese Feld sollte einen der folgenden Werte enthalten: {0}. [{1}]",
-                            "notEnoughItems": "Die Mindestanzahl von Elementen ist {0}",
-                            "tooManyItems": "Die Maximalanzahl von Elementen ist {0}",
-                            "valueNotUnique": "Diese Werte sind nicht eindeutig",
-                            "notAnArray": "Keine Liste von Werten",
-                            "invalidDate": "Das Datum muss in folgender Form eingegben werden: {0}",
-                            "invalidEmail": "Ungültige e-Mail Adresse, z.B.: info@cloudcms.com",
-                            "stringNotAnInteger": "Eingabe ist keine Ganz Zahl.",
-                            "invalidIPv4": "Ungültige IPv4 Adresse, z.B.: 192.168.0.1",
-                            "stringValueTooSmall": "Die Mindestanzahl von Zeichen ist {0}",
-                            "stringValueTooLarge": "Die Maximalanzahl von Zeichen ist {0}",
-                            "stringValueTooSmallExclusive": "Die Anzahl der Zeichen muss größer sein als {0}",
-                            "stringValueTooLargeExclusive": "Die Anzahl der Zeichen muss kleiner sein als {0}",
-                            "stringDivisibleBy": "Der Wert muss durch {0} dividierbar sein",
-                            "stringNotANumber": "Die Eingabe ist keine Zahl",
-                            "invalidPassword": "Ungültiges Passwort.",
-                            "invalidPhone": "Ungültige Telefonnummer, z.B.: (123) 456-9999",
-                            "invalidPattern": "Diese Feld stimmt nicht mit folgender Vorgabe überein {0}",
-                            "stringTooShort": "Dieses Feld sollte mindestens {0} Zeichen enthalten",
-                            "stringTooLong": "Dieses Feld sollte höchstens {0} Zeichen enthalten"
-                        }
-                    },
-                    "callbacks": {
-                        "required": function () {
-                            var fieldEl = this.getFieldEl();
-                            // required fields get a little star in their label
-                            var label = $(fieldEl).find("label.alpaca-control-label");
-                            $('<span class="alpaca-icon-required glyphicon glyphicon-star"></span>').appendTo(label);
-                        }
-                    }
-                },
-
-                "viewx": {
-                    "parent": "bootstrap-edit",
-                    "layout": {
-                        "template": "twoColumnGridLayout",
-                        "bindings": {
-                            "name": "leftcolumn",
-                            "birthday": "leftcolumn",
-                            "city": "rightcolumn"
-                        }
-                    },
-                    "templates": {
-                        "twoColumnGridLayout": '<div class="row"><div class="col-md-6" id="leftcolumn"></div><div class="col-md-6" id="rightcolumn"></div></div>'
-                    }
-                }
-            });
-        });
-
-        // show modal dialog
-        this.ui.editArticleEntry.modal({backdrop: true});
-
-        console.log("View has been rendered");
-    },
-    saveClicked: function (e) {
-        var self = this;
-        self.ui.errorMessage.addClass("hidden");
-
-        this.model.save().done(function () {
-            self.ui.editArticleEntry.modal('hide');
-            location.reload();
-        }).fail(function (req) {
-            if (req.responseText && req.responseText.substr(0, 14) != "<!DOCTYPE html") {
-                console.log("Saving event failed: " + req.responseText);
-            }
-            self.ui.errorMessage.text(req.status + " " + req.statusText).removeClass("hidden");
-        });
-    }
-
-});
 
 $(function () {
     $(".articleListItem").click(function () {
         var clickedElement = $(this);
         var id = clickedElement.attr('data-id');
-        var model = new ArticleItem({id: id});
-
-        // When waiting for the completion of fetch and then
-        // giving this model to the view we don't get the initial
-        // modelChanged event.
-        // This is by intention to have the save button enabled only
-        // if the user changed values in the UI.
-        model.fetch({
-            success: function () {
-                console.log("Article fetched");
-                new ArticleItemView({model: model}).render();
-            }
+        articleEditApp.controller('articleEditController', function ($scope, Entry) {
+            $scope.loadArticle(id);
         });
+
+        ui.editArticleEntry.on('shown.bs.modal', function (e) {
+            console.log("Modal dialog showed");
+        });
+
+        // show modal dialog
+        ui.editArticleEntry.modal({backdrop: true});
+
+        console.log("showing modal dialog...");
+
     });
 });
+
+function saveClicked() {
+    ui.errorMessage.addClass("hidden");
+
+    this.model.save().done(function () {
+        ui.editArticleEntry.modal('hide');
+        location.reload();
+    }).fail(function (req) {
+        if (req.responseText && req.responseText.substr(0, 14) != "<!DOCTYPE html") {
+            console.log("Saving event failed: " + req.responseText);
+        }
+        ui.errorMessage.text(req.status + " " + req.statusText).removeClass("hidden");
+    });
+}
