21ed19844c7f901deb522895c9408dd324afc704
Anton Schegg <as5@schegg.de>
2015-05-15 09:58:15 +0200

    social share

--- public/javascripts/applySocialShare.js
+++ public/javascripts/applySocialShare.js
@@ -1,4 +1,18 @@
 $(".socialshare").socialSharePrivacy({
+    services: {
+        facebook: {
+            perma_option: 'off',
+            dummy_img: '/lib/socialshareprivacy/images/dummy_facebook.png'
+        },
+        twitter: {
+            perma_option: 'off',
+            dummy_img: '/lib/socialshareprivacy/images/dummy_twitter.png'
+        },
+        gplus: {
+            perma_option: 'off',
+            dummy_img: '/lib/socialshareprivacy/images/dummy_gplus.png'
+        }
+    },
     'language': 'de',
     'lang_path': '/lib/socialshareprivacy/lang/',
     uri: function (context) {
