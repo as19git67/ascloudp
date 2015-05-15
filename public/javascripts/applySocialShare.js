c62400b6ec941dabb6267bb627cb2dc477baf75f
Anton Schegg <as5@schegg.de>
2015-05-15 09:38:52 +0200

    social share

---
+++ public/javascripts/applySocialShare.js
@@ -0,0 +1,8 @@
+$(".socialshare").socialSharePrivacy({
+    uri : function(context) {
+        var url = window.location.pathname;
+        var id  = $(context).parents(".articleListItem").find("a").attr("id");
+        var url = url + "/" + id;
+        return url;
+    }
+});
