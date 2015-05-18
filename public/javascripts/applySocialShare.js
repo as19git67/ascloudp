4dda9a191a9edee4cc986cf40f7d5850bbc11faf
Anton Schegg <as5@schegg.de>
2015-05-18 15:48:42 +0200

    social-likes flat without zeroes

--- public/javascripts/applySocialShare.js
+++ public/javascripts/applySocialShare.js
@@ -1,14 +1,16 @@
 $(function () {
     var elements = $('.social-likesX');
 
-    var s, url, href, d;
+    var s, url, href, d, t;
     for (var i = 0; i < elements.length; i++) {
         s = $(elements[i]);
+        t = s.attr('data-title');
         d = s.attr('data-url');
         href = window.location.href.replace(window.location.hash, "");
         url = href + d;
         s.socialLikes({
-            url: url
+            url: url,
+            title: t
         });
     }
 });
